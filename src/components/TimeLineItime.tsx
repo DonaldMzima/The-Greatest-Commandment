cb579951629cbb5ec04af570999ccbba47bf0ef5
Donald Mzima <donaldmzima8@gmail.com>
2024-07-04 10:21:22 +0200

    ref: add react icons

--- src/components/TimeLineItime.tsx
+++ src/components/TimeLineItime.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import LoadingSpinner from "./LoadingSpinner";
 import Fuse from "fuse.js";
 import { useTimelineData } from "../../utils/https";
@@ -28,14 +29,15 @@ const Timeline: React.FC = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <div className="mb-4">
+      <div className="mb-4 relative">
         <input
           type="text"
           placeholder="Search..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded text-black"
+          className="w-full p-2 border border-gray-300 rounded text-black pl-10"
         />
+        <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
       </div>
       <div className="mb-4">
         {searchTerm && (
