ba3d51cacb1a3e84af949565539b98ec4596bc24
Donald Mzima <donaldmzima8@gmail.com>
2024-07-04 13:18:32 +0200

    ref: footer color

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-cover text-white p-4 mt-8">
+    <footer className="bg-cover text-gray-500 p-4 mt-8">
       <div className="max-w-7xl mx-auto text-center">
         <p>&copy; 2024 Developed by Donald Mzima.</p>
       </div>
